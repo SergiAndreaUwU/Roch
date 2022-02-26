4eed82589d534c08cb0ac36136bf5755d0514ae6
Sergio Salazar <sso@bbpingod.com>
2022-02-26 00:58:13 -0600

    adding header layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import styles from "./header.module.sass"
+import React from "react";
+import styles from "./header.module.sass";
 
 const Header = () => {
   return (
-    <div className={styles.header}>header</div>
-  )
-}
+    <div className={styles.header}>
+      <div className={styles.containerTitle}>
+        <h1>POS Qubit - Roch</h1>
+      </div>
+      <div className={styles.containerButtonsHeader}>
+        <div className={styles.left}>a</div>
+        <div className={styles.right}>b</div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
