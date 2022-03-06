961a1c1e1ab8a1730e0cc79a7161be3f4aeea5f7
Sergio Salazar <sso@bbpingod.com>
2022-03-06 14:00:17 -0600

    added currency transaction login at shopInfo

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import styles from "./menu.module.sass";
 import Right from "./Right/right";
@@ -10,7 +11,21 @@ const Menu = ({
   reduxAddToShopListWithIndex,
   reduxDeleteFromShopListWithIndex,
   reduxResetShopList,
+  shopList,
 }) => {
+  const [paymentInfo, setPaymentInfo] = useState({
+    totalPrice: 0.0,
+    payedWith: 250.0,
+  });
+  useEffect(() => {
+    const totalPrice = shopList.reduce(
+      (acc, el) => acc + el.price * el.quantity,
+      0
+    );
+
+    setPaymentInfo({ ...paymentInfo, totalPrice: totalPrice.toFixed(2) });
+  }, [shopList]);
+
   return (
     <div className={styles.menu}>
       {/* left */}
@@ -54,28 +69,29 @@ const Menu = ({
             pharetra sem. Nulla porta ipsum quis condimentum dictum. Proin
             auctor porttitor commodo. Pellentesque ut justo augue. Nullam a
             aliquet eros, in imperdiet eros. Integer aliquam aliquam nibh vitae
-            cursus. Duis id rutrum erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            vitae urna malesuada, ultricies enim non, ullamcorper lacus. Nunc
-            feugiat bibendum purus, dapibus fringilla elit volutpat sit amet.
-            Quisque laoreet, augue vel pulvinar pharetra, nisi erat egestas
-            mauris, vel efficitur massa massa ac nunc. Praesent pellentesque
-            neque nec nunc dignissim mollis. Nam viverra neque vel aliquet
-            volutpat. Nullam gravida nisi ut enim facilisis, mollis pellentesque
-            urna consequat. Pellentesque a orci non nulla facilisis imperdiet.
-            Praesent auctor ligula sit amet orci dignissim vulputate. Sed
-            elementum turpis et nunc vulputate, eget bibendum nisl suscipit.
-            Nullam vitae lectus at enim posuere fermentum. Quisque semper
-            imperdiet leo, at condimentum metus sollicitudin eget. Etiam luctus
-            felis sed diam finibus venenatis. Sed faucibus ex non nisl euismod
-            ornare. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. Duis suscipit tincidunt quam ut
-            sagittis. Nulla velit turpis, viverra at velit vel, lacinia bibendum
-            nunc. Aenean id sapien scelerisque, venenatis tortor et, porta nisi.
-            Vivamus convallis suscipit nulla vel vestibulum. Integer sit amet
-            venenatis turpis. Proin facilisis finibus velit, non viverra lacus
-            aliquet ut. Interdum et malesuada fames ac ante ipsum primis in
-            faucibus. Pellentesque at pulvinar arcu, condimentum ultrices ex.
-            Duis tristique eget lacus nec eleifend. Integer porta elit at turpis
+            cursus. Duis id rutrum erat.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Aliquam vitae urna malesuada, ultricies enim non,
+            ullamcorper lacus. Nunc feugiat bibendum purus, dapibus fringilla
+            elit volutpat sit amet. Quisque laoreet, augue vel pulvinar
+            pharetra, nisi erat egestas mauris, vel efficitur massa massa ac
+            nunc. Praesent pellentesque neque nec nunc dignissim mollis. Nam
+            viverra neque vel aliquet volutpat. Nullam gravida nisi ut enim
+            facilisis, mollis pellentesque urna consequat. Pellentesque a orci
+            non nulla facilisis imperdiet. Praesent auctor ligula sit amet orci
+            dignissim vulputate. Sed elementum turpis et nunc vulputate, eget
+            bibendum nisl suscipit. Nullam vitae lectus at enim posuere
+            fermentum. Quisque semper imperdiet leo, at condimentum metus
+            sollicitudin eget. Etiam luctus felis sed diam finibus venenatis.
+            Sed faucibus ex non nisl euismod ornare. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Duis suscipit tincidunt quam ut sagittis. Nulla velit turpis,
+            viverra at velit vel, lacinia bibendum nunc. Aenean id sapien
+            scelerisque, venenatis tortor et, porta nisi. Vivamus convallis
+            suscipit nulla vel vestibulum. Integer sit amet venenatis turpis.
+            Proin facilisis finibus velit, non viverra lacus aliquet ut.
+            Interdum et malesuada fames ac ante ipsum primis in faucibus.
+            Pellentesque at pulvinar arcu, condimentum ultrices ex. Duis
+            tristique eget lacus nec eleifend. Integer porta elit at turpis
             convallis ultrices eget nec enim. Sed eu felis a sem imperdiet
             ultrices sed eget tortor. Quisque ac vestibulum eros. Nulla
             tincidunt eget turpis sed cursus. Nunc et lorem fringilla, aliquet
@@ -95,7 +111,13 @@ const Menu = ({
           </div>
           <div class={styles.reservedSpace}></div>
         </div>
-        <div class={styles.shopInfo}></div>
+        <div class={styles.shopInfo}>
+          Paga: {paymentInfo.payedWith}
+          <br />
+          Cambio: {paymentInfo.payedWith - paymentInfo.totalPrice >=0? (paymentInfo.payedWith - paymentInfo.totalPrice).toFixed(2):"0"}
+          <br />
+          Total:{paymentInfo.totalPrice}
+        </div>
       </div>
       {/* end left */}
       <Right reduxResetShopList={reduxResetShopList} />
@@ -107,6 +129,7 @@ function mapStateToProps(state) {
   return {
     categories: state.categories,
     selectedCatalogueCategory: state.selectedCatalogueCategory,
+    shopList: state.shopList,
   };
 }
 
